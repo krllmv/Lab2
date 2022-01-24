5c90cd0c41d7af4fd895486e7ec0c03dd4a995ac
krllmv <kirill.efimov1280@mail.ru>
2022-01-24 17:48:22 +0300

    commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib> // для system 1324
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+    system("pause"); // Только для тех, у кого MS Visual Studio м324
+    return 0;
+}
